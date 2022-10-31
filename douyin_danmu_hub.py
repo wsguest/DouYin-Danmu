3461a3f260e8dc6e6ce6307b1472c8df9df5e63a
wsguest <seai@tom.com>
2022-10-31 11:42:10 +0800

    reformat
    format code for reading

--- douyin_danmu_hub.py
+++ douyin_danmu_hub.py
@@ -4,8 +4,8 @@ import websockets
 import logging
 from playwright.async_api import async_playwright
 
-clients = set() # 客户端
-debug = True # 调试状态
+clients = set()  # 客户端
+debug = True  # 调试状态
 
 # 日志输出设置
 logger = logging.getLogger("ws_hub")
@@ -16,13 +16,17 @@ handler_console = logging.StreamHandler()
 handler_console.setFormatter(fmt)
 logger.addHandler(handler_console)
 
+
 # websocket 服务
-async def handler(websocket, path):
+async def ws_handler(websocket, path):
+    logger.info(f"Client connected {websocket.remote_address[0]}:{websocket.remote_address[1]}{path}")
     clients.add(websocket)
     try:
         await websocket.wait_closed()
     finally:
         clients.remove(websocket)
+
+
 
 # 广播消息
 async def broadcast_message(msg: dict):
@@ -32,62 +36,64 @@ async def broadcast_message(msg: dict):
         await c.send(data)
     return ""
 
-# 注入代码，先隐藏播放器，再监听聊天栏
-# 以抖音为例其他平台做对应修改
-script = """
-document.querySelector('.basicPlayer').remove();
-const targetNode = document.getElementsByClassName('webcast-chatroom___items')[0].firstChild;
-const callback = function(mutationsList, observer) {
-    for(const mutation of mutationsList) {
-            if(mutation.addedNodes.length > 0) {
-                const node = mutation.addedNodes[0].firstChild;
-                if(node.childNodes.length < 2)
-                    continue;
-                const name = node.childNodes[1].innerText;
-                const content = node.lastChild.innerText;
-                console.log(name + ': ' + content);
-                window.on_message({data:{nickname:name, content:content, uid:0, senderlevel:10}, timestamp:0});
-            }
-        }
-    };
-    
-const config = {attributes: false, childList: true, subtree: true, characterData: false};
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
-"""
 
-# 启动浏览器
-async def pw_douyin(room_id):
-    url = f"https://live.douyin.com/{room_id}"
+# 注入的代码，先隐藏播放器，再监听聊天栏 以抖音为例其他平台做对应修改
+script_douyin = """
+    document.querySelector('.basicPlayer').remove(); // 隐藏播放器
+    const chat_list = document.getElementsByClassName('webcast-chatroom___items')[0].firstChild;
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for(const mutation of mutationsList) {
+                if(mutation.addedNodes.length > 0) {
+                    const node = mutation.addedNodes[0].firstChild;
+                    if(node.childNodes.length < 3)
+                        continue;
+                    const level = node.firstChild.innerHTML.match(/user_grade_level_v5_(\d+)\.png/i)[1]
+                    const name = node.childNodes[1].innerText;
+                    const content = node.lastChild.innerText;
+                    // windows.host_callback 为保留函数名称，要与callback_name保持一致，参数成员可以根据需要任意修改
+                    window.host_callback({data:{nickname:name, content:content, uid:0, level:level}, timestamp:0});
+                }
+            }
+        });
+    const config = {attributes: false, childList: true, subtree: true, characterData: false};
+    observer.observe(chat_list, config);
+    """
+callback_name = "on_message"  # 回调函数签名，用于浏览器和代码通信
+script_douyin = script_douyin.replace("host_callback", callback_name)
+
+# 启动浏览器，打开直播间，注入代码
+async def open_room(room_url, javascript):
     check_interval = 5
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=not debug, channel="msedge")
+        browser = await p.chromium.launch(headless=not debug, channel="msedge")  # 启动Edge浏览器
         context = await browser.new_context()
-        await context.expose_function("on_message", broadcast_message)
+        await context.expose_function(callback_name, broadcast_message)
         page = await context.new_page()
         while True:
-            if page.is_closed() or page.url != url:
-                if page.is_closed():
+            if page.is_closed() or page.url != room_url:
+                if page.is_closed():  # 保持打开
                     page = await context.new_page()
-                await page.goto(url)
-                await page.evaluate(script)
+                await page.goto(room_url)
+                await page.evaluate(javascript)
 
             await asyncio.sleep(check_interval)
-            await broadcast_message({"state": "alive", "interval": check_interval})
+            await broadcast_message({"state": "alive", "interval": check_interval})  # 解决客户端同步读取模式下的挂起问题
+
 
 # 运行hub
-def run_hub_forever(ws_port, room_id):
+def run_hub_forever(listen_port, room_url, script):
     try:
-        ws_future = websockets.serve(handler, "localhost", ws_port)
-        douyin_future = pw_douyin(room_id)
-        logger.info(f"Hub server listen at: {ws_port}")
-        asyncio.gather(douyin_future, ws_future)
+        ws_future = websockets.serve(ws_handler, "localhost", listen_port)
+        logger.info(f"Hub server listen at: {listen_port}")
+        room_future = open_room(room_url, script)
+        asyncio.gather(room_future, ws_future)
         asyncio.get_event_loop().run_forever()
     except Exception as ex:
         logger.error(f"Hub server exception: {ex}")
 
 
 if __name__ == "__main__":
-    room = "918495818136"  # douyin room id
-    port = 88  #  websocket port
-    run_hub_forever(port, room)
+    room_douyin = "https://live.douyin.com/208754230598"  # douyin room url
+    ws_port = 88  # websocket listen port
+    
+    run_hub_forever(ws_port, room_douyin, script_douyin)
